429a9a16368011ec1178a9202cbf2a43876cc568
MriquelmeCPHCJA <marcelo.riquelme@cph-cja.cl>
2025-03-22 22:11:17 -0300

    refactor: make config/db.js

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const CategoryProducts = require('../models/CategoryProducts');
+const Product = require('../models/Product');
+const User = require('../models/User');
+const Cart = require('../models/Cart');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI) // Configuración de la conexión a la base de datos
+
+        // Inicialización de los modelos de colecciones
+        await CategoryProducts.init()
+        await Product.init()
+        await User.init()
+        await Cart.init()
+
+        console.log('Conexión a la base de datos exitosa');
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
